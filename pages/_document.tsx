d1e0bcba95042cbdeb4a0fc78e9df1fbe941df66
solaxds <peter@artbybloc.com>
2022-04-12 09:28:21 +0100

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,17 @@ export default function Document() {
     <Html>
       <Head />
       <body className='bg-gradient-to-r from-gray-200 via-orange-50 via-orange-100 via-green-50 to-gray-200'>
+        <link
+          as="font"
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Jua&display=swap"
+        />
+
+        <link
+          as="font"
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@600&display=swap"
+        />
         <Main />
         <NextScript />
       </body>
